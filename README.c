c1f46213676fa90347001afba8e2036463abc969
dandapataishwarya <dandapataishwarya@gmail.com>
2016-07-08 14:26:35 +0530

    finish

---
+++ README.c
@@ -0,0 +1,16 @@
+# positiveornegative
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter a number");
+scanf("%d",@n)
+if(n>0)
+{
+printf("Number is positive");
+}
+else
+{
+printf("negative");
+}
+}
